52095074dd92f61c29849f573f44785cd42f6959
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-28 17:16:54 +0300

    Task 1 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace task1
+﻿using System;
+using task03;
+
+namespace task1
 {
     internal static class Program
     {
         private static void Main()
         {
+            var price = new Price("name", "shop", 0);
+            Console.WriteLine(price.GetType());
+            Console.WriteLine(price.ToString());
         }
     }
 }
