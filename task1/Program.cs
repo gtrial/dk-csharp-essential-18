9ce273acea78ec02fa29e657408f1947e474aebe
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-05-28 17:19:42 +0300

    Task 2 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using task03;
+using task2;
 
 namespace task1
 {
@@ -8,8 +9,10 @@ namespace task1
         private static void Main()
         {
             var price = new Price("name", "shop", 0);
-            Console.WriteLine(price.GetType());
             Console.WriteLine(price.ToString());
+
+            var item = new PublicClass();
+            Console.WriteLine(item.ToString());
         }
     }
 }
---
+++ task2/PublicClass.cs
@@ -0,0 +1,7 @@
+namespace task2
+{
+    public class PublicClass
+    {
+        
+    }
+}
